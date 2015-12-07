bcfde0c06cc2e2a4ee1a2a9b767009b010490cff
laperry1 <laperry7706@gmail.com>
2015-12-06 21:35:56 -0500

    Remove unnecessay code: PRODUCT_COPY_FILES += \      packages/apps/Viper4Android/system/etc/audio_effects.conf:system/etc/audio_effects.conf
    audio_effects.conf already copied from device/oneplus/bacon

--- viperfx.mk
+++ viperfx.mk
@@ -1,9 +1,6 @@
 PRODUCT_PACKAGES += \
     Viper4Android
 
-PRODUCT_COPY_FILES += \
-     packages/apps/Viper4Android/system/etc/audio_effects.conf:system/etc/audio_effects.conf
-
 PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
     lpa.releaselock=false \
